db9831082c44e09d60f42798942ebec347cfdbba
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-04 16:08:06 +0900

    Headerコンポーネント編集

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Header from "./components/Header/page";
+import {Header} from "./components/Header/Header";
 
 export default function Home() {
   return (
-    <h1>こんにちは</h1>
-    <Header />
+    <div>
+      <Header />
+      <h1>こんにちは</h1>
+    </div>
   );
 }
