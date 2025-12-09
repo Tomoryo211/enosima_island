7bdd8d66ae7e8b1a0d4dde01ec4472bd1c3d86d4
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-09 16:38:39 +0900

    画像削除/Imageタグ追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@
 import {Header} from "./components/Header/Header";
 import  styles  from "./index.module.scss";
 import Image from "next/image";
-import Top from "./Images/mainvisual.jpg";
 
 
 export default function Home() {
   return(
     <div className={styles.body}>
       <Header />
-      <Image src={Top} alt="mainvisual" />
+      
     </div>
   )
 }
