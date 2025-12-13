020f07c474cf3a5a717c15762e8403bf89f9dbbf
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-13 21:54:31 +0900

    お試しで画像で乗っけてみる

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 import {Header} from "./components/Header/Header";
 import  styles  from "./index.module.scss";
 import Image from "next/image";
+import back from "./Images/mainBJ.png";
 
 
 export default function Home() {
   return(
     <div className={styles.body}>
       <Header />
-      
+      <Image src={back}  alt="背景"/>
     </div>
   )
 }
