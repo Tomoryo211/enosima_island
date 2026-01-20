fabbd4441e46f499e60bcee4a6e1c455e9fce996
Tomoryo211 <ryo.tomo.211@gmail.com>
2026-01-20 13:54:01 +0900

    btnスタイル変更

--- app/components/Shop/shop.tsx
+++ app/components/Shop/shop.tsx
@@ -8,15 +8,15 @@ export default function Shop(){
         <>
         <div>
             <h2 className={style.shop}>現在営業中店舗</h2>
-            <section className={style.input_area}>
-                <form action="./search/" className={style.input}>
-                    <input type="text" id="movie" name="q" className={style.input_field}/>
-                    <button type="submit" className={style.btn}>検索</button>
-                </form>
-            </section>
+                <section className={style.input_area}>
+                    <form action="./search/" className={style.input}>
+                        <input type="text" id="movie" name="q" className={style.input_field}/>
+                        <button type="submit" className={style.btn}>検索</button>
+                    </form>
+                </section>
             <div className={style.Eat_btn}>
-                <button>飲食</button>
-                <button>体験</button>
+                <button className={style.Select_btn}>飲食</button>
+                <button className={style.Select_btn}>体験</button>
             </div>
         </div>
         </>
