c1154329d28f83a79c5666b4c598d311c974a3b3
Tomoryo211 <ryo.tomo.211@gmail.com>
2026-01-26 16:36:10 +0900

    ロゴ追加/header部分スタイル変更/flex

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import styles from "./Header.module.scss";
+import Image from "next/image";
+import enosima from "../../Images/enosima.jpg";
 
 export function Header() {
     return (
         <>
             <div className={styles.header}>
+                <a href="/"><Image 
+                    src={enosima}
+                    alt="ロゴ"
+                    className={styles.img}
+                    priority 
+                /></a>
                 <ul className={styles.Header_List}>
                     <li><a href="/">Topページ</a></li>
                     <li><a href="/shops">お店紹介</a></li>
