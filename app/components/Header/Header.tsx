77e119c9fb22c2854a32bcfb6e2371ef735e87fe
Tomoryo211 <ryo.tomo.211@gmail.com>
2026-02-05 14:27:55 +0900

    header編集

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
                     priority
                 /></a>
                 <ul className={styles.Header_List}>
-                    <li><a href="/">Topページ</a></li>
+                    <li><a href="/">topページ</a></li>
                     <li><a href="/shops">お店紹介</a></li>
                     <li><a href="/route-search">ルート提案</a></li>
                 </ul>
